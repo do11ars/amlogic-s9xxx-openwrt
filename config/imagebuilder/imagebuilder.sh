3540798469feddd540a77b24e2ab0d960d77d651
do11ars <169393033+do11ars@users.noreply.github.com>
2025-10-05 13:35:28 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -180,7 +180,7 @@ rebuild_firmware() {
         acpid attr base-files bash bc blkid block-mount blockd bsdtar btrfs-progs busybox bzip2 openssh-sftp-server \
         cgi-io chattr comgt comgt-ncm containerd coremark coreutils coreutils-base64 coreutils-nohup \
         coreutils-truncate curl dosfstools dumpe2fs e2freefrag e2fsprogs \
-        exfat-mkfs f2fs-tools f2fsck fdisk gawk getopt git gzip hostapd-common iconv iw iwinfo jq \
+        exfat-mkfs f2fs-tools f2fsck fdisk gawk getopt git git-http gzip hostapd-common iconv iw iwinfo jq \
         jshn kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script liblucihttp \
         liblucihttp-lua losetup lsattr lsblk lscpu mkf2fs mount-utils openssl-util parted \
         perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perlbase-utf8 \
